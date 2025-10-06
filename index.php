ed4669fea0154ae376d63cecfdbff4ab8c27be5b
Hog rider 2.0 <marekszymak@gmail.com>
2025-10-06 11:45:22 +0200

    golem
    gole,

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pl">
+<head>
+<link rel="stylesheet" href="style.css">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<title>Województwo Małopolskie</title>
+</head>
+<body>
+
+<section class="hero">
+  <header>
+    <h1>Województwo Małopolskie</h1>
+  </header>
+
+  <div class="top-row">
+   
+    <a href="https://rezero.fandom.com/wiki/Ferris" class="card white-block">Biały blok </a>
+
+ 
+    <div class="bubble">Krótka informacja</div>
+
+   
+    <a href="https://www.youtube.com" class="card">
+      <div class="map-thumb"></div>
+      <div>Mapa Polski </div>
+    </a>
+    <div class="hero2">Krótka informacja</div>
+  
+</div>
+  </div>
+</section>
+
+</body>
+</html>
